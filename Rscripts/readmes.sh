35564484e8d5b12b85b305fc35943608c82610be
Andrew <a.a.m.macdonald@gmail.com>
2013-09-29 16:10:44 -0700

    READMEs for all the fieldsitesgit status

--- Rscripts/readmes.sh
+++ Rscripts/readmes.sh
@@ -5,5 +5,56 @@ cp explanation.Rmd ../Experimental.Schedules/Cardoso/README.Rmd
 cd ../Experimental.Schedules/Cardoso/
 Rscript -e "library(knitr); knit(input='README.Rmd',output='README.md')"
 #pandoc
-#~/.cabal/bin/pandoc -s -S ../Experimental.Schedules/Cardoso/README.md -o ../Experimental.Schedules/Cardoso/README.pdf --latex-engine=xelatex
+#~/.cabal/bin/pandoc -s -S README.md -o README.pdf --latex-engine=xelatex
+cd ../../Rscripts
+
+
+# copy explanation.Rmd to readme
+cp explanation.Rmd ../Experimental.Schedules/Colombia/README.Rmd
+# knit
+cd ../Experimental.Schedules/Colombia/
+Rscript -e "library(knitr); knit(input='README.Rmd',output='README.md')"
+#pandoc
+#~/.cabal/bin/pandoc -s -S README.md -o README.pdf --latex-engine=xelatex
+cd ../../Rscripts
+
+
+# copy explanation.Rmd to readme
+cp explanation.Rmd ../Experimental.Schedules/CostaRica/README.Rmd
+# knit
+cd ../Experimental.Schedules/CostaRica/
+Rscript -e "library(knitr); knit(input='README.Rmd',output='README.md')"
+#pandoc
+#~/.cabal/bin/pandoc -s -S README.md -o README.pdf --latex-engine=xelatex
+cd ../../Rscripts
+
+
+
+
+# copy explanation.Rmd to readme
+cp explanation.Rmd ../Experimental.Schedules/Argentina/README.Rmd
+# knit
+cd ../Experimental.Schedules/Argentina/
+Rscript -e "library(knitr); knit(input='README.Rmd',output='README.md')"
+#pandoc
+#~/.cabal/bin/pandoc -s -S README.md -o README.pdf --latex-engine=xelatex
+cd ../../Rscripts
+
+
+# copy explanation.Rmd to readme
+cp explanation.Rmd ../Experimental.Schedules/FrenchGuiana/README.Rmd
+# knit
+cd ../Experimental.Schedules/FrenchGuiana/
+Rscript -e "library(knitr); knit(input='README.Rmd',output='README.md')"
+#pandoc
+#~/.cabal/bin/pandoc -s -S README.md -o README.pdf --latex-engine=xelatex
+cd ../../Rscripts
+
+# copy explanation.Rmd to readme
+cp explanation.Rmd ../Experimental.Schedules/Macae/README.Rmd
+# knit
+cd ../Experimental.Schedules/Macae/
+Rscript -e "library(knitr); knit(input='README.Rmd',output='README.md')"
+#pandoc
+#~/.cabal/bin/pandoc -s -S README.md -o README.pdf --latex-engine=xelatex
 cd ../../Rscripts

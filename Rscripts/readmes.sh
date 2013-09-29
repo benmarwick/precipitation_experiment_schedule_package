6b6e7cea09dc906818f0ca78752c4febbea13ac2
Andrew <a.a.m.macdonald@gmail.com>
2013-09-29 16:02:15 -0700

    fixing figures in readmes

--- Rscripts/readmes.sh
+++ Rscripts/readmes.sh
@@ -2,6 +2,8 @@
 # copy explanation.Rmd to be Cardoso's readme
 cp explanation.Rmd ../Experimental.Schedules/Cardoso/README.Rmd
 # knit
-Rscript -e "library(knitr); knit(input='../Experimental.Schedules/Cardoso/README.Rmd',output='../Experimental.Schedules/Cardoso/README.md')"
+cd ../Experimental.Schedules/Cardoso/
+Rscript -e "library(knitr); knit(input='README.Rmd',output='README.md')"
 #pandoc
 #~/.cabal/bin/pandoc -s -S ../Experimental.Schedules/Cardoso/README.md -o ../Experimental.Schedules/Cardoso/README.pdf --latex-engine=xelatex
+cd ../../Rscripts

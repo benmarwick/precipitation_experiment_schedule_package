3bdeb7ea7e7e4d9be62552179af740cb3874aa82
Andrew <a.a.m.macdonald@gmail.com>
2013-09-29 15:56:33 -0700

    Cardoso figures made

--- Rscripts/readmes.sh
+++ Rscripts/readmes.sh
@@ -2,6 +2,6 @@
 # copy explanation.Rmd to be Cardoso's readme
 cp explanation.Rmd ../Experimental.Schedules/Cardoso/README.Rmd
 # knit
-Rscript -e "library(knitr); knit(input='../Experimental.Schedules/Cardoso/README.Rmd',input='../Experimental.Schedules/Cardoso/README.md')"
+Rscript -e "library(knitr); knit(input='../Experimental.Schedules/Cardoso/README.Rmd',output='../Experimental.Schedules/Cardoso/README.md')"
 #pandoc
 #~/.cabal/bin/pandoc -s -S ../Experimental.Schedules/Cardoso/README.md -o ../Experimental.Schedules/Cardoso/README.pdf --latex-engine=xelatex
